8d602729be43ff07f0192186fa1624c090d5a7c9
kevin beier <kabeier@gmail.com>
2026-08-13 16:46:47 -0400

    part 1 dryrun

--- server/task_api/settings.py
+++ server/task_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'task_app',
     'user_app',
 ]
@@ -72,7 +74,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'task_api.wsgi.application'
 
-
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+}
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
--- server/user_app/urls.py
+++ server/user_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", Info.as_view()),
     path("create/", CreateUser.as_view()),
     path("login/", LogIn.as_view()),
-    path("logout/", LogOut.as_view())
+    path("logout/", LogOut.as_view()),
+    path("refresh/", RefreshView.as_view()),
 ]
